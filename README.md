c4d1216a0ef4390dca6d82bd0bd11ff5fb75ea38
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2020-02-27 16:31:56 -0500

    feat: Add more outputs

--- README.md
+++ README.md
@@ -39,9 +39,22 @@ Terraform module to deploy aws-health-exporter on kubernetes.
 
 | Name | Description |
 |------|-------------|
+| deployment\_annotations | Map of annotations that are configured on the deployment. |
+| deployment\_labels | Map of labels that are configured on the deployment. |
 | deployment\_name | Name of the deployment created by the module. |
 | grafana\_dashboards | List of strings, each one representing a separate grafana dashboard. |
+| image\_name | Name of the docker image used for the cloudwatch-exporter container. |
+| image\_pull\_policy | Image pull policy defined on the cloudwatch-exporter container. |
+| image\_version | Tag of the docker image used for the cloudwatch-exporter container. |
+| namespace | Name of the namespace in which the resources have been deployed. |
+| secret\_annotations | Map of annotations that are configured on the secret. |
+| secret\_labels | Map of labels that are configured on the secret. |
 | secret\_name | Name of the secret created by the module. |
+| selector\_labels | Map of the labels that are used as selectors. |
+| service\_annotations | Map of annotations that are configured on the service. |
+| service\_labels | Map of labels that are configured on the service. |
 | service\_name | Name of the service created by the module. |
+| service\_port | Port number of the service port. |
+| service\_port\_name | Name of the service port. |
 
 <!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,104 @@
+#####
+# Global
+#####
+
+output "selector_labels" {
+  description = "Map of the labels that are used as selectors."
+  value       = element(concat(kubernetes_service.this.*.spec.0.selector, [{}]), 0)
+}
+
+output "grafana_dashboards" {
+  description = "List of strings, each one representing a separate grafana dashboard."
+  value       = var.enabled ? [file("${path.module}/templates/grafana-dashboards/aws-health-status.json")] : []
+}
+
+output "image_name" {
+  description = "Name of the docker image used for the cloudwatch-exporter container."
+  value       = var.enabled ? var.image_name : ""
+}
+
+output "image_version" {
+  description = "Tag of the docker image used for the cloudwatch-exporter container."
+  value       = var.enabled ? var.image_version : ""
+}
+
+output "image_pull_policy" {
+  description = "Image pull policy defined on the cloudwatch-exporter container."
+  value       = var.enabled ? var.image_pull_policy : ""
+}
+
+#####
+# Deployment
+#####
+
 output "deployment_name" {
   description = "Name of the deployment created by the module."
   value       = element(concat(kubernetes_deployment.this.*.metadata.0.name, [""]), 0)
 }
+
+output "deployment_annotations" {
+  description = "Map of annotations that are configured on the deployment."
+  value       = element(concat(kubernetes_deployment.this.*.metadata.0.annotations, [{}]), 0)
+}
+
+output "deployment_labels" {
+  description = "Map of labels that are configured on the deployment."
+  value       = element(concat(kubernetes_deployment.this.*.metadata.0.labels, [{}]), 0)
+}
+
+#####
+# Secret
+#####
 
 output "secret_name" {
   description = "Name of the secret created by the module."
   value       = element(concat(kubernetes_secret.this.*.metadata.0.name, [""]), 0)
 }
 
+output "secret_annotations" {
+  description = "Map of annotations that are configured on the secret."
+  value       = element(concat(kubernetes_secret.this.*.metadata.0.annotations, [{}]), 0)
+}
+
+output "secret_labels" {
+  description = "Map of labels that are configured on the secret."
+  value       = element(concat(kubernetes_secret.this.*.metadata.0.labels, [{}]), 0)
+}
+
+#####
+# Service
+#####
+
 output "service_name" {
   description = "Name of the service created by the module."
   value       = element(concat(kubernetes_service.this.*.metadata.0.name, [""]), 0)
 }
 
-output "grafana_dashboards" {
-  description = "List of strings, each one representing a separate grafana dashboard."
-  value       = var.enabled ? [file("${path.module}/templates/grafana-dashboards/aws-health-status.json")] : []
+output "service_port" {
+  description = "Port number of the service port."
+  value       = var.enabled ? local.service_port : ""
+}
+
+output "service_port_name" {
+  description = "Name of the service port."
+  value       = var.enabled ? "http" : ""
+}
+
+output "service_annotations" {
+  description = "Map of annotations that are configured on the service."
+  value       = element(concat(kubernetes_service.this.*.metadata.0.annotations, [{}]), 0)
+}
+
+output "service_labels" {
+  description = "Map of labels that are configured on the service."
+  value       = element(concat(kubernetes_service.this.*.metadata.0.labels, [{}]), 0)
+}
+
+#####
+# Namespace
+#####
+
+output "namespace" {
+  description = "Name of the namespace in which the resources have been deployed."
+  value       = element(concat(kubernetes_deployment.this.*.metadata.0.namespace, [""]), 0)
 }
